689c4975a720bcce0548838a0ee056b3be50624f
Darshan Parmar <140134153+Mynamedarshan@users.noreply.github.com>
2024-06-30 18:05:08 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountNumber = 12445
+let accountEmail = "darshan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gujrat"
+let accountState 
+
+
+// accountNumber = 3434; // not allowed bcz its const
+
+accountEmail = "test@gmail.com"
+accountPassword = "444444"
+accountCity = "Banglore"
+
+
+
+// console.log(accountNumber);
+
+console.table([accountEmail, accountNumber, accountPassword, accountState, accountCity]);
